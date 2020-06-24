29b17f6d2737a0abb2ee78feec83a446f2d9b842
Joseph Montoya <jtmontoya26@gmail.com>
2020-06-24 12:00:07 -0700

    Several components and styling added to Price_Block. Refactored to implement styled components.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import Carousel from './Carousel.jsx';
 import Price_Block from'./Price_Block.jsx';
+import SliderThumb from './SliderThumb.jsx'
+import styled from 'styled-components';
+
+const Test = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 1185px;
+  max-height: 1000px;
+  margin: 0 auto;
+  background: f4f4f4;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -32,10 +44,14 @@ class App extends React.Component {
   render() {
     if (this.state.isLoaded === true) {
       return(
-        <div >
-          <Carousel product={this.state} />
-          <Price_Block product={this.state} />
+        <div>
+          <Test>
+            <Carousel product={this.state} />
+            <Price_Block product={this.state} />
+          </Test>
+          <SliderThumb product={this.state} />
         </div>
+
       )
     } else {
       return(
