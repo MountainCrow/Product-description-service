caeb836e5da96b6d5eca6ac2da556493f8c083fd
Joseph Montoya <jtmontoya26@gmail.com>
2020-06-26 08:42:12 -0700

    Add working star-ratings component

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import CreateStar from './CreateStar.jsx';
+import {FaStar} from 'react-icons/fa';
+import {FaStarHalfAlt} from 'react-icons/fa';
 import styled from 'styled-components';
 
 var StarRating = (props) => {
 
-  
+  const solidStar = Math.floor(props.data.rating)
+  console.log("solidStar:" , solidStar)
+
+  const hasHalfStar = false;
+  if (solidStar < props.data.rating) {
+    hasHalfStar = true;
+  }
+
   return(
-    <div>{Result}</div>
+    <div>
+      {[...Array(5)].map((star, index) => {
+        if (index < solidStar) {
+          return(
+            <FaStar key={index} style={{color: '#cc1618'}}/>
+          )
+        } else if (hasHalfStar && index < 5) {
+          return (
+            <FaStarHalfAlt key={index} style={{color: '#cc1618'}}/>
+          )
+        } else if (index < 5) {
+          return (
+            <FaStar key={index} style={{color: '#959595'}}/>
+          )
+        }
+      })}
+    </div>
   )
 }
 
