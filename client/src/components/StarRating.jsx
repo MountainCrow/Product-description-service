56282b7305bd6f97b305759f6c5aa7b8a8b5179c
Joseph Montoya <jtmontoya26@gmail.com>
2020-06-25 17:29:11 -0700

    Quick Save

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -4,12 +4,7 @@ import styled from 'styled-components';
 
 var StarRating = (props) => {
 
-  const Result = () => {
-    for (let i = 0; i < props.data; i++) {
-      <CreateStar key={i} data={'FaStar'}/>
-    }
-  }
-
+  
   return(
     <div>{Result}</div>
   )
