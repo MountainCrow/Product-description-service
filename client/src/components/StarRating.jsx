c063fe070dee9109a79b2cea96a1ea9bf0a4d892
Joseph Montoya <jtmontoya26@gmail.com>
2020-06-26 08:53:01 -0700

    Added feature which indicates the total number of reviews

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -3,6 +3,17 @@ import {FaStar} from 'react-icons/fa';
 import {FaStarHalfAlt} from 'react-icons/fa';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledTotal = styled.span`
+  padding-bottom: 4px;
+  padding-left: 5px;
+`;
+
 var StarRating = (props) => {
 
   const solidStar = Math.floor(props.data.rating)
@@ -14,23 +25,26 @@ var StarRating = (props) => {
   }
 
   return(
-    <div>
-      {[...Array(5)].map((star, index) => {
-        if (index < solidStar) {
-          return(
-            <FaStar key={index} style={{color: '#cc1618'}}/>
-          )
-        } else if (hasHalfStar && index < 5) {
-          return (
-            <FaStarHalfAlt key={index} style={{color: '#cc1618'}}/>
-          )
-        } else if (index < 5) {
-          return (
-            <FaStar key={index} style={{color: '#959595'}}/>
-          )
-        }
-      })}
-    </div>
+    <Wrapper>
+      <label>
+        {[...Array(5)].map((star, index) => {
+          if (index < solidStar) {
+            return(
+              <FaStar key={index} style={{color: '#cc1618'}}/>
+            )
+          } else if (hasHalfStar && index < 5) {
+            return (
+              <FaStarHalfAlt key={index} style={{color: '#cc1618'}}/>
+            )
+          } else if (index < 5) {
+            return (
+              <FaStar key={index} style={{color: '#959595'}}/>
+            )
+          }
+        })}
+      </label>
+      <StyledTotal>(6)</StyledTotal>
+    </Wrapper>
   )
 }
 
