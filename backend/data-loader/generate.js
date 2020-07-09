bbfc56d11badfdcea07d027079d52b7497a02f6a
lmodin <lisamodin@live.com>
2020-07-09 12:12:40 -0700

    data loader#

---
+++ backend/data-loader/generate.js
@@ -0,0 +1,19 @@
+
+
+//write a script that will create random data for the table
+  //columns we need:
+  //color - string, ideally includes a three digit number to later translate it
+  //description -- string, just a sentence long
+  //gender == string: male, female, unisex
+  //image --string (url)
+  //name -- string
+  //price -- numeric (should have two decimal places)
+  //rating -- number 1-5
+  //size -- string
+  //style -- string
+  // totalRatings -- number from 0 to something big
+  //type -- string
+  //productId -- string, unique based on the product itself
+    //will eventually be used to load the data as the url
+//find a way to save the output in a csv format
+
